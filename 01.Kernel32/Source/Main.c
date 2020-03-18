7160e28a93fdf1cb3643390219aa8e12c9cb90ee
SangHoon Choi <csh0052@gmail.com>
2020-03-18 08:39:16 -0700

    p.233

--- 01.Kernel32/Source/Main.c
+++ 01.Kernel32/Source/Main.c
@@ -1,3 +1,4 @@
+
 #include "Types.h"
 
 void kPrintString( int iX, int iY, const char* pcString );
@@ -6,8 +7,12 @@ void kPrintString( int iX, int iY, const char* pcString );
  *  아래 함수는 C 언어 커널의 시작 부분임
  *      반드시 다른 함수들 보다 가장 앞쪽에 존재해야 함
  */
+BOOL kInitializeKernel64Area( void );
 void Main( void )
 {
+    DWORD i;
+    kPrintString(0,4,"IA-32e Kernel area Intiailization Complete");
+    
     kPrintString( 0, 3, "C Language Kernel Started~!!!" );
 
     while( 1 ) ;
@@ -29,4 +34,29 @@ void kPrintString( int iX, int iY, const char* pcString )
     {
         pstScreen[ i ].bCharactor = pcString[ i ];
     }
+}
+BOOL kInitializeKernel64Area( void )
+{
+    DWORD* pdwCurrentAddress;
+    
+    // 초기화를 시작할 어드레스인 0x100000(1MB)을 설정
+    pdwCurrentAddress = ( DWORD* ) 0x100000;
+    
+    // 마지막 어드레스인 0x600000(6MB)까지 루프를 돌면서 4바이트씩 0으로 채움
+    while( ( DWORD ) pdwCurrentAddress < 0x600000 )
+    {        
+        *pdwCurrentAddress = 0x00;
+
+        // 0으로 저장한 후 다시 읽었을 때 0이 나오지 않으면 해당 어드레스를 
+        // 사용하는데 문제가 생긴 것이므로 더이상 진행하지 않고 종료
+        if( *pdwCurrentAddress != 0 )
+        {
+            return FALSE;
+        }
+        
+        // 다음 어드레스로 이동
+        pdwCurrentAddress++;
+    }
+    
+    return TRUE;
 }
